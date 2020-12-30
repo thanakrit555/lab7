568892d403a3372d9b3508c78924ffbebf19819d
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2020-12-30 19:18:38 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -9,3 +9,39 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    int age,hei,pro;
+    cout<<"Enter your age: ";
+    cin>>age;
+    
+    if(age<=20){
+        cout<<"Enter your height: ";
+        cin>>hei;
+        if(hei<=160){
+          cout<<"Your status = FRIEND"  ;
+        }else{
+            cout<<"Enter your property: ";
+            cin>>pro;
+            if(pro>69000000){
+            cout<<"Your status = MARRIED";  
+            }else{
+            cout<<"Your status = ONE-NIGHT-STAND";    
+            }
+        }
+    }
+    
+    if(age<=30){
+      cout<<"Enter your property: ";
+      cin>>pro;
+      if(pro>10000000){
+       cout<<"Your status = BEST FRIEND";   
+      }else{
+       cout<<"Your status = UNFRIEND";   
+      }
+    }
+    
+}
